048ba68e211aeb831b58883dc3c725dde375b985
Ben Braun <benjamin.braun2003@gmail.com>
2025-09-14 01:19:02 -0400

    Add Basic Dropdown Functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,15 @@
 import "../styles.css";
+
+
+const container = document.querySelector('.container');
+const dropdownButton = document.querySelector('.dropdown--button');
+const dropdownMenu = document.querySelector('.dropdown--menu')
+
+dropdownButton.addEventListener("click", (e) => {
+
+    console.log("dropdown button pressed");
+    dropdownMenu.classList.toggle("visible");
+
+});
+
+
